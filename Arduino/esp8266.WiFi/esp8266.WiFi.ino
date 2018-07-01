d021585ed9b9cdcd758c67ebdfdb893bd50f16ce
LopezDP <davidplopez@live.com>
2018-07-01 13:25:00 -0400

    Started esp8266 and ThingSpeak API work.

---
+++ Arduino/esp8266.WiFi/esp8266.WiFi.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+
+float field;
+
+String apiKey = "";
+
+SoftwareSerial
+
+
+void setup() {
+  // put your setup code here, to run once:
+    serial1.begin(115200);
+  serial1.println("AT");
+  delay(2000);
+
+  serial1.println("AT+RST");
+  delay(2000);
+
+  serial.println("AT
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.
+
+
+}
